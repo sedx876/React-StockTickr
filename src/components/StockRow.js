22d1f92bfaa866598606f3320f8a0b03d95c4150
sedx876 <swatkins876@gmail.com>
2020-11-25 06:32:25 -0500

    added config/iex.js

--- src/components/StockRow.js
+++ src/components/StockRow.js
@@ -2,13 +2,30 @@ import React, { Component } from 'react'
 
 class StockRow extends Component {
 
+  constructor(props){
+    super(props)
+    this.state = {
+      data: {}
+    }
+  }
+
+  componentDidMount(){
+    this.setState({
+      data: {
+        price: 5,
+        date: '12-07-12',
+        time: '09:45'
+      }
+    })
+  }
+
   render() {
     return(
       <tr>
         <td>{this.props.ticker}</td>
-        <td>Yo</td>
-        <td>Wassup</td>
-        <td>Hola</td>
+        <td>{this.state.data.price}</td>
+        <td>{this.state.data.date}</td>
+        <td>{this.state.data.time}</td>
       </tr>
     )
   }
---
+++ src/config/iex.js
@@ -0,0 +1,4 @@
+export const iex = {
+  api_token: 'pk_f576449c7ec94a2d84c1a78ebc96ec3b',
+  base_url: 'https://cloud.iexapis.com/stable'
+}
